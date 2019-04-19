cf6c68de2fd9136499d141e0628af0f8afa7a67e
kjayaprakash <kjayaprakash4028@gmail.com>
2019-04-18 21:31:50 -0400

    added missing autowired annotation

--- src/main/java/com/jp/shipping/controller/ShippingAddressController.java
+++ src/main/java/com/jp/shipping/controller/ShippingAddressController.java
@@ -2,6 +2,7 @@ package com.jp.shipping.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.jp.shipping.service.ShippingAddressService;
 @RestController
 public class ShippingAddressController {
 	
+	@Autowired
 	private ShippingAddressService shippingAddressService;
 
 	@GetMapping("/users/{userId}/shippingAddress")
